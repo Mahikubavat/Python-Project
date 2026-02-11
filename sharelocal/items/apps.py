9e176d97715e7f66a5ba793fa37847e874454c12
Heer Maheshwari <heer16102006@gmail.com>
2026-02-11 20:10:39 +0530

    Add verbose_name to ItemsConfig class
    Added verbose_name attribute for better representation.

--- sharelocal/items/apps.py
+++ sharelocal/items/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class ItemsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'items'
+    verbose_name = 'Items Management'
+
