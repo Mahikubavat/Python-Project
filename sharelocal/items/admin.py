f994cb294a0ef4b693fe366324d3e07846b0be52
Mahikubavat <mahikubavat5@gmail.com>
2026-02-12 20:52:37 +0530

    upto manage requests

--- sharelocal/items/admin.py
+++ sharelocal/items/admin.py
@@ -1,33 +1,5 @@
 from django.contrib import admin
 from .models import Item
+# Register your models here.
 
-
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    """
-    Admin configuration for Item model.
-    """
-    list_display = ('title', 'owner', 'category', 'item_type', 'price', 'is_available', 'created_at')
-    list_filter = ('category', 'item_type', 'is_available', 'created_at')
-    search_fields = ('title', 'description', 'owner__username')
-    readonly_fields = ('created_at', 'updated_at')
-    fieldsets = (
-        ('Item Information', {
-            'fields': ('title', 'description', 'category', 'image'),
-        }),
-        ('Item Details', {
-            'fields': ('item_type', 'price'),
-        }),
-        ('Ownership & Status', {
-            'fields': ('owner', 'is_available'),
-        }),
-        ('Timestamps', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',),
-        }),
-    )
-
-    def get_readonly_fields(self, request, obj=None):
-        if obj:  # Editing existing object
-            return self.readonly_fields + ('owner',)
-        return self.readonly_fields
+admin.site.register(Item)
--- sharelocal/items/apps.py
+++ sharelocal/items/apps.py
@@ -4,5 +4,3 @@ from django.apps import AppConfig
 class ItemsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'items'
-    verbose_name = 'Items Management'
-
